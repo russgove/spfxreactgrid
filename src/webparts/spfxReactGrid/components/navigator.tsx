6db3fea630874a0fff712f3033d622989026bbed
Russell Gove <RussellGove@gmail.com>
2016-12-16 11:11:10 -0500

    qw

--- src/webparts/spfxReactGrid/components/navigator.tsx
+++ src/webparts/spfxReactGrid/components/navigator.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import * as React from "react";
 
 export interface INavigatorProps extends React.Props<any> {
   testid?: string;
@@ -7,7 +7,7 @@ export interface INavigatorProps extends React.Props<any> {
 export default function Navigator({
 
   children = null,
-  testid = ''
+  testid = ""
 }: INavigatorProps) {
   return (
     <nav
